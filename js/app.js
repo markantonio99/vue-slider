dfbb8d3d15a539ea6e7b5b6953cd2bbf5a8792b5
“markantonio99” <markomakkia@gmail.com>
2023-02-20 22:51:03 +0100

    salv 2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,39 @@
-
-<div id="app">{{ message }}</div>
-
-<script>
-  const { createApp } = Vue
-
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
+const app = Vue.createApp({
+  data() {
+    return {
+      images: [
+        './immagini/imm2.png',
+        './immagini/imm3.png',
+        './immagini/imm4.png',
+        './immagini/imm5.png',
+        './immagini/imm6.png',
+      ],
+      currentIndex: 0
+    }
+  },
+  computed: {
+    inserisciImmagine() {
+      return this.images[this.currentIndex]
+    },
+    disableNonePrev() {
+      return this.currentIndex === 0
+    },
+    disableNoneNext() {
+      return this.currentIndex === this.images.length - 1
+    }
+  },
+  methods: {
+    btnNext() {
+      if (this.currentIndex < this.images.length - 1) {
+        this.currentIndex++
+      }
+    },
+    btnPrev() {
+      if (this.currentIndex > 0) {
+        this.currentIndex--
       }
     }
-  }).mount('#app')
-</script>
+  }
+});
+
+app.mount('#app');
